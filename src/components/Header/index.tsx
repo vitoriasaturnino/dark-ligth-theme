ddc50122bd936043ec4c7fb413598a4434d80887
vitoriasaturnino <vim.saturnino@gmail.com>
2022-11-08 11:43:50 -0300

    feature: create compoent header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,7 @@
+import { ContainerStyle } from "./styles";
+
+export function Header(){
+    return (
+      <ContainerStyle>Hellow Horld</ContainerStyle>
+    );
+}
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const ContainerStyle = styled.div`
+  height: 60px;
+  background: #7159c1;
+  color: #FFFFFF;
+  display: flex;
+  align-items: center;
+  padding: 0 30px;
+`;
