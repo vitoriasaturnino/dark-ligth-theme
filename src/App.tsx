2ad31c99f01b99deb681ac20aed95cf818178112
vitoriasaturnino <vim.saturnino@gmail.com>
2022-11-15 14:42:20 -0300

    fix: add type to functions state and useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider, DefaultTheme } from 'styled-components';
 import ligth from './styles/themes/ligth';
 import dark from './styles/themes/dark';
 import GlobalStyle from './styles/global';
@@ -6,7 +6,7 @@ import Header from './components/Header';
 import usePersistedState from './utils/usePersistedState';
 
 function App() {
-  const [theme, setTheme] = usePersistedState('theme', ligth);
+  const [theme, setTheme] = usePersistedState<DefaultTheme>('theme', ligth);
 
   const toggleTheme = () => {
     setTheme(theme.title === 'ligth' ? dark : ligth);
